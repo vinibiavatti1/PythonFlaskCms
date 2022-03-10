2f69f460e5b5b9035b6fca7b093017dfb603c251
viniciusSonae <viniciusb@parceiro.sonae.pt>
2022-03-10 22:37:13 +0000

    urls part 1

---
+++ project/records/urls.py
@@ -0,0 +1,22 @@
+"""
+URLs records module.
+"""
+from project.models.label_value_model import LabelValueModel
+
+
+###############################################################################
+# Registry
+###############################################################################
+
+
+layouts: list[LabelValueModel] = [
+    LabelValueModel(
+        label='Default Layout',
+        value='default_layout.html'
+    ),
+    LabelValueModel(
+        label='Empty Layout (Without navbar, footers, etc.)',
+        value='empty_layout.html'
+    ),
+    # Add more...
+]
